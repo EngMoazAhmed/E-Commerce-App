6ad9fe1b0a786c89a5f665c79ea92c0ef12ab222
Moaz <67188321+MeYo0o@users.noreply.github.com>
2021-10-19 11:25:45 +0200

    Add 4 Checkout Steps Skeleton views

---
+++ lib/view/checkout/address.body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddressBody extends StatelessWidget {
+  const AddressBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
---
+++ lib/view/checkout/delivery.body.dart
@@ -0,0 +1,42 @@
+import 'package:e_commerce_app/constants/checkout.dart';
+import 'package:e_commerce_app/constants/colors.dart';
+import 'package:e_commerce_app/core/view_model/checkout_view_model.dart';
+import 'package:e_commerce_app/view/widgets/checkout/custom_radio_tile.dart';
+import 'package:e_commerce_app/view/widgets/common/custom_text.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class DeliveryBody extends StatelessWidget {
+  const DeliveryBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<CheckoutViewModel>(builder: (controller) {
+      return Column(
+        children: [
+          SizedBox(height: Get.height * 0.1),
+          const CustomRadioTile(
+            title: 'Standard Delivery',
+            subtitle: 'Order will be delivered between 3 - 5 business days.',
+            deliveryValue: DeliveryType.standardDelivery,
+          ),
+          SizedBox(height: Get.height * 0.05),
+          const CustomRadioTile(
+            title: 'Next Day Delivery',
+            subtitle: 'Place your order before 6pm and your items will be '
+                'delivered the next day.',
+            deliveryValue: DeliveryType.nextDayDelivery,
+          ),
+          SizedBox(height: Get.height * 0.05),
+          const CustomRadioTile(
+            title: 'Nominated Delivery',
+            subtitle:
+                'Pick a particular date from the calendar and order will be '
+                'delivered on selected date.',
+            deliveryValue: DeliveryType.nominatedDelivery,
+          ),
+        ],
+      );
+    });
+  }
+}
